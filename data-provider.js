cb36acb1887ea611d5b96556abe553041e66c9ce
Olga Miliukova <cogito_ergo_summ@mail.ru>
2019-12-06 10:11:46 +0300

    Improved data provider cache and fixed environment declaration

--- data-provider.js
+++ data-provider.js
@@ -3,25 +3,33 @@ const path = require('path');
 
 module.exports = {
   cachedData: {},
-  getData(key) {
+  isFreshData(key) {
     if (!(key in this.cachedData)) {
-      const file = path.join(__dirname, `data/${key}.json`);
-      const data = fs.readFileSync(file, { encoding: 'utf8' });
-
-      this.cachedData[key] = JSON.parse(data);
+      return false;
     }
 
-    return this.cachedData[key];
+    const { dtime } = this.cachedData[key];
+    const { mtime } = fs.statSync(this.getPathData(key));
+
+    return mtime < dtime;
+  },
+  getPathData(key) {
+    return path.join(__dirname, `data/${key}.json`);
+  },
+  getData(key) {
+    if (!this.isFreshData(key)) {
+      const data = fs.readFileSync(this.getPathData(key), { encoding: 'utf8' });
+
+      this.cachedData[key] = {
+        data: JSON.parse(data),
+        dtime: new Date(),
+      };
+    }
+
+    return this.cachedData[key].data;
   },
   getDataById(key, id) {
-    return this.getData(key).reduce((model, next) => {
-      const { _id = '' } = next;
-      if (_id === id) {
-        return next;
-      }
-
-      return model;
-    }, null);
+    return this.getData(key).find(({ _id = '' }) => _id === id);
   },
   getCards() {
     return this.getData('cards');
